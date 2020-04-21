2df4aa96ee400c0524c5101843f53e19ae81f0fe
joshbutkovic <joshbutkovic@gmail.com>
2020-04-21 15:03:10 -0700

    Deploy

---
+++ precache-manifest.9721b84e1d4aafefb4e21d674ef6db8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab2e73fb802588d9d29a",
+    "url": "/weather-view/static/css/main.e775079b.chunk.css"
+  },
+  {
+    "revision": "ab2e73fb802588d9d29a",
+    "url": "/weather-view/static/js/main.ab2e73fb.chunk.js"
+  },
+  {
+    "revision": "f8b532a9c6ddf2987de4",
+    "url": "/weather-view/static/js/1.f8b532a9.chunk.js"
+  },
+  {
+    "revision": "8e0dc539feb1af21e739",
+    "url": "/weather-view/static/js/runtime~main.8e0dc539.js"
+  },
+  {
+    "revision": "374344147cff8606f835d752944d8570",
+    "url": "/weather-view/static/media/light-grey-terrazzo.37434414.png"
+  },
+  {
+    "revision": "ebd6fa11f1d6620996b2f385de0e32f3",
+    "url": "/weather-view/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/weather-view/precache-manifest.9721b84e1d4aafefb4e21d674ef6db8e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/weather-view/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
